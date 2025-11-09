0f578aad1910391a68d2b99a1d8bcd4f120149b6
ningunFernando <117299803+ningunFernando@users.noreply.github.com>
2025-11-09 16:37:19 +0100

    all views created

---
+++ app/src/main/java/com/example/hangman_kotlin/GameplayActivity.kt
@@ -0,0 +1,20 @@
+package com.example.hangman_kotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class GameplayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_gameplay)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
